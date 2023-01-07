4e4bef431a15da8fe79a8631ea896de80a382ff0
championeer <zhao.qianli@gmail.com>
2023-01-07 17:06:19 +0800

    update

--- config.el
+++ config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-vibrant)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -84,8 +85,8 @@
 ;;--END--
 ;;--设置TODO--
 ;;(t)代表快捷字母；!代表时间戳；@代表一个有时间戳的记录笔记
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "IN-PROGRESS(i)" "WAITING(w)" "CANCELED(c@/!)" "|" "DONE(d@/!)" "DELEGATED(e!)")))
+(after! org (setq org-todo-keywords
+      '((sequence "TODO(t)" "IN-PROGRESS(i)" "WAITING(w)" "DELEGATED(e!)" "|" "DONE(d@/!)" "CANCELED(c@/!)"))))
 ;;
 ;;任务完成时，记录时间及log
 (setq org-log-done 'note)
@@ -202,7 +203,7 @@
   (re-search-backward "\\* LIFELOG"))
 
 
-(setq org-capture-templates
+(after! org (setq org-capture-templates
       '(("t" "TASK" entry (file+headline "work/task.org" "Tasks")
          "* TODO %i%? [/] :@work: \n %U\n From: %a\n")
         ("n" "NOTE" entry (file "note.org")
@@ -220,7 +221,7 @@
         ("e" "EVENT" entry
          (file+function "everyday.org"
                         my-org-goto-last-event-headline)
-         "* %i%? \n%T")))
+         "* %i%? \n%T"))))
 
 (defun newday ()
   (interactive)
@@ -287,9 +288,10 @@
 ;;自动刷新buffer
 ;;（global-auto-revert-mode t）
 (setq auto-revert-use-notify nil)
-;;设置Emacs启动窗口大小
-(add-to-list 'default-frame-alist '(height . 24))
-(add-to-list 'default-frame-alist '(width . 80))
+;;设置Emacs启动窗口大小(default Frame size)
+;;(add-to-list 'default-frame-alist '(height . 24))
+;;(add-to-list 'default-frame-alist '(width . 80))
+(pushnew! initial-frame-alist '(width . 220) '(height . 80))
 ;;设置windows
 (setq evil-vsplit-window-right t
       evil-split-window-below t)
