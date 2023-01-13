4a3de000d798d2bcdaa796d2ccb7cbe6dce17acf
qianli <zhao.qianli@gmail.com>
2023-01-14 00:06:28 +0800

    屏蔽yasnippet和org-roam的一个冲突提示

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-file-name-style 'relative-to-project)
+ '(doom-modeline-major-mode-icon t)
+ '(doom-modeline-modal-icon nil)
+ '(warning-suppress-log-types
+   '(((yasnippet zombie))
+     ((yasnippet zombie))
+     ((yasnippet zombie))
+     (defvaralias)))
+ '(warning-suppress-types '(((yasnippet zombie)) ((yasnippet zombie)) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(doom-modeline-buffer-modified ((t (:foreground "orange")))))
