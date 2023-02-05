3db454e637d9d32786f3f199cf7b7fc6464ef13f
qianli <zhao.qianli@gmail.com>
2023-02-05 16:14:25 +0800

    start using modus-theme

--- packages.el
+++ packages.el
@@ -45,6 +45,8 @@
 (package! calibredb)
 ;;
 (package! keyfreq)
+;;
+(package! modus-themes)
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
